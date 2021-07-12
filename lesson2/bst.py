ac5d434d1621034b40fee2bb9976a911ae46c658
yangxin6 <2362813136@qq.com>
2021-07-13 07:24:46 +0800

    kNN最邻近搜索 radiusNN最邻近搜索

--- lesson2/bst.py
+++ lesson2/bst.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+from lesson2.result_set import KNNResultSet, RadiusNNResultSet
+
 
 class Node:
     def __init__(self, key, value=-1):
@@ -92,6 +94,7 @@ def search_1nn(root: Node, key, worst_distance=float('inf')):
             return search_1nn(root.left, key, worst_distance)
 
     elif key > root.key:
+        # 遍历右子树
         if math.fabs(key - root.key) < worst_distance:
             if root.left is None:
                 return root
@@ -102,13 +105,51 @@ def search_1nn(root: Node, key, worst_distance=float('inf')):
             return search_1nn(root.right, key, worst_distance)
 
 
+# kNN Search
+def knn_search(root: Node, result_set: KNNResultSet, key):
+    if root is None:
+        return False
+    result_set.add_point(math.fabs(root.key - key), root.value)
+    if key <= root.key:
+        # 遍历左子树
+        if knn_search(root.left, result_set, key):
+            return False
+        elif math.fabs(root.key - key) < result_set.worstDist():
+            return knn_search(root.right, result_set, key)
+    elif key > root.key:
+        # 遍历右子树
+        if knn_search(root.right, result_set, key):
+            return False
+        elif math.fabs(root.key - key) < result_set.worstDist():
+            return knn_search(root.left, result_set, key)
+
+
+# radius Search
+def radius_search(root: Node, result_set: RadiusNNResultSet, key):
+    if root is None:
+        return False
+    result_set.add_point(math.fabs(root.key - key), root.value)
+    if key <= root.key:
+        # 遍历左子树
+        if radius_search(root.left, result_set, key):
+            return False
+        elif math.fabs(root.key - key) < result_set.worstDist():
+            return radius_search(root.right, result_set, key)
+    elif key > root.key:
+        # 遍历右子树
+        if radius_search(root.right, result_set, key):
+            return False
+        elif math.fabs(root.key - key) < result_set.worstDist():
+            return radius_search(root.left, result_set, key)
+
+
 def main():
     db_size = 100
     k = 5
     radius = 2.0
     data = np.random.permutation(db_size).tolist()
     # 临近搜索把 2 删掉
-    print(data)
+    data.remove(11)
     root = None
     for i, point in enumerate(data):
         root = insert(root, point, i)
@@ -127,6 +168,19 @@ def main():
     value = search_1nn(root, 11)
     print(value)
 
+    # kNN最邻近搜索
+    knn_result_set = KNNResultSet(capacity=k)
+    knn_search(root, knn_result_set, 20)
+    print(knn_result_set)
+    for i in knn_result_set.dist_index_list:
+        print(data[i.index])
+
+    # radius search
+    radius_result_set = RadiusNNResultSet(radius=radius)
+    radius_search(root, radius_result_set, 20)
+    print(radius_result_set)
+    for i in radius_result_set.dist_index_list:
+        print(data[i.index])
 
 if __name__ == '__main__':
     main()
